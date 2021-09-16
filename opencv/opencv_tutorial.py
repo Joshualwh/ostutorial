5946935cfb5a44e2986424df7da0502a78bac94c
Shohei Fujii <fujii.shohei@gmail.com>
2021-09-16 14:53:22 +0900

    Add opencv data

--- opencv/opencv_tutorial.py
+++ opencv/opencv_tutorial.py
@@ -1,11 +1,11 @@
 import numpy as np
 import cv2
 
-# Load an color image in grayscale
-img = cv2.imread('messi.jpg',0)
-print img
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
 
-# Display an image
+# Draw a diagonal blue line with thickness of 5 px
+cv2.line(img,(0,0),(511,511),(255,0,0),5)
+
 cv2.imshow('image',img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
