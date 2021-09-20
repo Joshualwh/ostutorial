73cda390318c33251fad939e7fa0f76e83050541
Shohei Fujii <fujii.shohei@gmail.com>
2021-09-20 16:30:14 +0900

    Pickup 20 boxes

--- ostutorial.py
+++ ostutorial.py
@@ -6,6 +6,7 @@ import time
 env = orpy.Environment()
 env.SetViewer('qtcoin')
 env.Load('/workspace/catkin_ws/src/osr_course_pkgs/osr_openrave/worlds/pick_and_place.env.xml')
+env.SetCollisionChecker(RaveCreateCollisionChecker(env, 'ode'))
 def create_box(T, color = [0, 0.6, 0]):
   box = orpy.RaveCreateKinBody(env, '')
   box.SetName('box')
@@ -19,9 +20,9 @@ def create_box(T, color = [0, 0.6, 0]):
 T = np.eye(4)
 container_center = np.array([0.4, 0.2, 0.195])
 # Destination
-T[:3, 3] = container_center + np.array([0, -0.5, 0])
+T[:3, 3] = container_center + np.array([0, -0.4, 0])
 destination0 = create_box(T, color = [0, 0, 0.6])
-T[:3, 3] = container_center + np.array([0, -0.6, 0])
+T[:3, 3] = container_center + np.array([0, -0.5, 0])
 destination1 = create_box(T, color = [0, 0, 0.6])
 # Generate random box positions
 boxes = []
@@ -72,11 +73,14 @@ ikmodel = orpy.databases.inversekinematics.InverseKinematicsModel(robot, iktype=
 if not ikmodel.load():
   ikmodel.autogenerate()
 
+number_of_boxes_placed = 0
+number_of_boxes_deleted = 0
+
 number_of_boxes = 39
-while number_of_boxes > 37:
+while number_of_boxes > 10:
   
   need_to_turn = 0
-  raw_input('Press Enter to start process')
+  # raw_input('Press Enter to start process')
   print ('Now is box'), number_of_boxes
   box = boxes[number_of_boxes]
   box_transform = box.GetTransform()
@@ -90,17 +94,16 @@ while number_of_boxes > 37:
   print effector_according_box
 
   ikparam = orpy.IkParameterization(effector_according_box, iktype)
-  from IPython.terminal import embed; ipshell=embed.InteractiveShellEmbed(config=embed.load_default_config())(local_ns=locals())
+  # from IPython.terminal import embed; ipshell=embed.InteractiveShellEmbed(config=embed.load_default_config())(local_ns=locals())
   solutions_with_col = manipulator.FindIKSolutions(ikparam, orpy.IkFilterOptions.CheckEnvCollisions)
   print solutions_with_col
   if len(solutions_with_col) == 0:
     solutions_without_col = manipulator.FindIKSolutions(effector_according_box, 0)
     print solutions_without_col
 
-  # raw_input ("wait")
-
   if len(solutions_with_col) == 0 and len(solutions_without_col) == 0:
     env.Remove(box)
+    number_of_boxes_deleted += 1
     print("bobian la...")
 
   if len(solutions_with_col) == 0 and len(solutions_without_col) != 0:
@@ -112,6 +115,7 @@ while number_of_boxes > 37:
 
     if len(solutions_with_col) == 0:
       env.Remove(box)
+      number_of_boxes_deleted += 1
       print("this one really bobian la...")
 
   if len(solutions_with_col) != 0:
@@ -127,7 +131,8 @@ while number_of_boxes > 37:
     robot.Grab(box)
     robot.WaitForController(0)
 
-    twist = np.array([0, 0, 0.001, 0, 0, 0])
+    move_up = 0.001 * (20-number_of_boxes/2)
+    twist = np.array([0, 0, move_up, 0, 0, 0])
     link_idx = [l.GetName() for l in robot.GetLinks()].index('robotiq_85_base_link')
     link_origin = robot.GetLink('robotiq_85_base_link').GetTransform()[:3,3]
     print link_origin
@@ -143,32 +148,57 @@ while number_of_boxes > 37:
       # raw_input('Press Enter for next differential IK step')
       time.sleep(0.1)
 
-    destination_stack = destination0.GetTransform()
-    effector_according_dest0 = np.eye(4)
-    effector_according_dest0[:3,:3] = np.dot(effector_transform[:3,:3], destination_stack[:3,:3])
-    effector_according_dest0[:3,3] = destination_stack[:3,3] + np.array([0,0,0.025*(40-number_of_boxes)])
+    effector_according_dest = np.eye(4)
+
+    if number_of_boxes > 24:
+      destination_stack = destination1.GetTransform()
+      effector_according_dest[:3,:3] = np.dot(effector_transform[:3,:3], destination_stack[:3,:3])
+      effector_according_dest[:3,3] = destination_stack[:3,3] + np.array([0,0,0.008]) + np.array([0,0,0.012*(40-number_of_boxes+number_of_boxes_deleted)])
+    
+    else:
+      destination_stack = destination0.GetTransform()
+      effector_according_dest[:3,:3] = np.dot(effector_transform[:3,:3], destination_stack[:3,:3])
+      effector_according_dest[:3,3] = destination_stack[:3,3] + np.array([0,0,0.008]) + np.array([0,0,0.012*(25-number_of_boxes)])
+    
+    print effector_according_dest
     if need_to_turn == 1:
       turn_effector_90 = matrixFromAxisAngle([0,0,np.pi/2])
-      effector_according_dest0[:3,:3] = np.dot(effector_according_dest0[:3,:3], turn_effector_90[:3,:3])
+      effector_according_dest[:3,:3] = np.dot(effector_according_dest[:3,:3], turn_effector_90[:3,:3])
 
-    ikparam = orpy.IkParameterization(effector_according_dest0, iktype)
+    ikparam = orpy.IkParameterization(effector_according_dest, iktype)
     solutions_for_dest = manipulator.FindIKSolutions(ikparam, orpy.IkFilterOptions.CheckEnvCollisions)
     print solutions_for_dest
 
-    qplace = solutions_for_dest[0]
-    traj = path_planner_and_exec(qplace)
-    controller = robot.GetController()
-    # print controller
-    controller.SetPath(traj)
-    robot.WaitForController(0)
+    solutions_for_dest_without_col = manipulator.FindIKSolutions(ikparam, 0)
+    print solutions_for_dest
 
-    taskprob.ReleaseFingers()
-    robot.WaitForController(0)
-    robot.Release(box)
-    robot.WaitForController(0)
+    if len(solutions_for_dest) == 0:
+      # raw_input('wait...')
+      taskprob.ReleaseFingers()
+      robot.WaitForController(0)
+      robot.Release(box)
+      robot.WaitForController(0)
+      env.Remove(box)
+      number_of_boxes_deleted += 1
+
+    if len(solutions_for_dest) != 0:
+      qplace = solutions_for_dest[0]
+      traj = path_planner_and_exec(qplace)
+      controller = robot.GetController()
+      # print controller
+      controller.SetPath(traj)
+      robot.WaitForController(0)
+      taskprob.ReleaseFingers()
+      robot.WaitForController(0)
+      robot.Release(box)
+      robot.WaitForController(0)
+      number_of_boxes_placed += 1
+
+    print('Number of boxes placed is'), number_of_boxes_placed
+    print('Number of boxes deleted is'), number_of_boxes_deleted
 
   number_of_boxes -= 1
-  raw_input('Next Box')
+  # raw_input('Next Box')
 
 while True:
   time.sleep(1)
