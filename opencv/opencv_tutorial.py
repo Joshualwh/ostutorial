07af2b6f69898f0005e4efc68a571319fac440e2
Joshualwh <joshualing0711@gmail.com>
2021-09-22 21:26:44 +0800

    Started osrobotics tutorial

--- opencv/opencv_tutorial.py
+++ opencv/opencv_tutorial.py
@@ -1,11 +1,40 @@
 import numpy as np
 import cv2
 
-# Create a black image
-img = np.zeros((512,512,3), np.uint8)
+def get_hole_center2d(image):
+  # Process images
+  # Read images
+  # image = cv2.imread('left_image.png')
+  # Blur images
+  image_blur = cv2.blur(image, (5,5))
+  # Canny Edge Detection 
+  image_edges = cv2.Canny(image_blur, 60, 120)
+  # Threshold is done to make the image binary
+  thresh, image_bw = cv2.threshold(image_edges, 80, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-# Draw a diagonal blue line with thickness of 5 px
-cv2.line(img,(0,0),(511,511),(255,0,0),5)
+  contours, hierarchy = cv2.findContours(image_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  contour_image = cv2.cvtColor(image_bw, cv2.COLOR_GRAY2RGB)
 
-cv2.imshow('image',img)
+  # cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 3)
+  # cv2.imshow('Contours', contour_image)
+
+  for i,cnt in enumerate(contours):
+    if len(np.squeeze(cnt)) > 5:
+      rect = cv2.minAreaRect(cnt)
+      (delta_u, delta_v) = rect[1]
+      diameter_pixel = max(delta_u,delta_v)
+      circularity = delta_u/delta_v if delta_v > delta_u else delta_v/delta_u
+      good_circularity = circularity > 0.8
+      good_diameter = diameter_pixel > 30
+      if good_circularity and good_diameter:
+        cv2.drawContours(contour_image, [cnt], 0, [0,0,255], 2)
+        cv2.imshow('image_contours.png',contour_image)
+        return True, rect
+  return False, False
+
+limage = cv2.imread('left_image.png')
+success, lres = get_hole_center2d(limage)
+
+# To stop it from ending directly
 cv2.waitKey(0)
+cv2.destroyAllWindows()
